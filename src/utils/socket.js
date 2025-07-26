e198b4138d300597e820526a5577ebf114e33312
assessment.hisan <assessment.hisan@gmail.com>
2025-07-26 22:10:43 +0530

    backurl in websockt

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 // src/utils/socket.js
 import { io } from "socket.io-client"
-const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:5000", {
+const socket = io(import.meta.env.VITE_SERVER_URL || "https://auction-backend-cmco.onrender.com/api/", {
     path: "/socket.io",
     transports: ["websocket", "polling"],
 }) // or your backend URL
